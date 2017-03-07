9dd5266a10783a7e161faeda1524311deee7e93c
cwsu <coco830418@gmail.com>
2017-03-07 18:59:05 +0800

    Update hw1_105753005.R
    make it can input and output depends on the commend

--- hw1/hw1_105753005.R
+++ hw1/hw1_105753005.R
@@ -1,10 +1,22 @@
-# run : Rscript hw1_105753005.R {input_file_name}
-#get the file name
+# run : Rscript hw1_105753005.R -files test.1.csv -out result.csv
+#get the input file name and output file name
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  stop("USAGE: Rscript hw1_105753005.R input", call.=FALSE)
-} else if (length(args)==1) {
-  i_fName <- args[1] 
+  stop("USAGE: Rscript hw1_105753005.R -files test.1.csv -out result.csv", call.=FALSE)
+} 
+i<-1 
+while(i < length(args))
+{
+  if(args[i] == "-files"){
+    i_f<-args[i+1]
+    i<-i+1
+  }else if(args[i] == "-out"){
+    o_f<-args[i+1]
+    i<-i+1
+  }else{
+    stop(paste("Unknown flag", args[i]), call.=FALSE)
+  }
+  i<-i+1
 }
 
 #read an CSV file and 
@@ -15,5 +27,5 @@ maxW <- round(max(i_f[2]),digits=2)
 maxH <- round(max(i_f[3]),digits=2)
 
 #output a CSV file 
-data = data.frame(set = i_fName, weight = maxW, height = maxH)
-write.csv(data , file = "result.csv" , row.names = FALSE)
+data = data.frame(set = i_f, weight = maxW, height = maxH)
+write.csv(data , file = o_f , row.names = FALSE)
